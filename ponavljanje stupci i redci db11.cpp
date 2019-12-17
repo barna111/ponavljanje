978648bdec2fbb601f871123cf9b949966f5321c
barna111 <58472151+barna111@users.noreply.github.com>
2019-12-17 14:04:28 +0100

    Add files via upload

---
+++ ponavljanje stupci i redci db11.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+	int main (){
+		int  n ;
+		 
+		 cout<<"Unesi jedan cijeli broj n veæi od 2 i manji od 100:";
+		 cin>>n
+		 
+		  for(i=n;i>2 && n<100,i++){
+		  	
+		  cout<<" "<<n<<" "<<n<<" "<<n;
+		  
+		  if(int i=n){
+		  	
+		  	cout<<" "<<i;
+		  }
+		 
+	    
+	    
+	    system("PAUSE");
+	    return 0; 
+	    
+	}
